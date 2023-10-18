b42b0968a2f9fa9668d11043bd944ac8d876f6fc
Pushkar Rajput <79901109+pushkarra@users.noreply.github.com>
2023-10-19 02:10:28 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -32,12 +32,12 @@ const getAllProducts = async(req,res) => {
         apiData = apiData.select(selectFix);
     }
 
-    let page = Number(req.query.page) || 1;
-    let limit = Number(req.query.limit) || 12;
+    // let page = Number(req.query.page) || 1;
+    // let limit = Number(req.query.limit) || 12;
 
-    let skip = (page - 1) * limit;
+    // let skip = (page - 1) * limit;
 
-    apiData = apiData.skip(skip).limit(limit);
+    // apiData = apiData.skip(skip).limit(limit);
 
     // console.log(queryObject);
 
